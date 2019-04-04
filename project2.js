7e5f0ec66aca1bdc655471baae2ca71bd2710fdb
taiwo Obafemi <taiwojonathann@gmail.com>
2019-04-04 17:23:25 +0100

    Initial commit

---
+++ project2.js
@@ -0,0 +1,28 @@
+new Vue ({
+
+	el: '#edit',
+
+	data: 	{
+		show_iconEdit: false,
+		message: 'Edit me.'
+	},
+
+	methods: {
+		hideIconEdit: function(){
+			this.show_iconEdit=false
+		},
+
+		toggleIconEdit: function(){
+			this.show_iconEdit= !this.show_iconEdit;
+		}
+	},
+	filters: {
+  capitalize: function (value) {
+    if (!value) return ''
+    value = value.toString()
+    return value.charAt(0).toUpperCase() + value.slice(1)
+  }
+}
+});
+
+ 
