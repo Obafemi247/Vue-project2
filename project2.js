63ca970689fa25b5454ca8ce87ada5c7709cd1fb
Obafemi247 <47661457+Obafemi247@users.noreply.github.com>
2019-04-05 12:17:21 +0100

    Update project2.js

--- project2.js
+++ project2.js
@@ -16,12 +16,12 @@ new Vue ({
 	},
 	
 	filters: {
-  capitalize: function (value) {
-    if (!value) return ''
-    value = value.toString()
-    return value.charAt(0).toUpperCase() + value.slice(1)
-  }
-}
+  	   capitalize: function (value) {
+   		 if (!value) return ''
+    		value = value.toString()
+    		return value.charAt(0).toUpperCase() + value.slice(1)
+  		}
+	}
 });
 
  
